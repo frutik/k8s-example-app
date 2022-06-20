cc2794a107fd31a62ff88f2ddbfb75ac922db0da
Andrew Kornilov <frutik@gmail.com>
2022-06-20 14:56:17 +0200

    Story I

---
+++ article_2/app.py
@@ -0,0 +1,19 @@
+import os
+import uvicorn
+
+from fastapi import FastAPI
+from fastapi.logger import logger
+
+
+app = FastAPI()
+
+
+@app.get("/hello")
+def hello():
+    logger.warning('Request!')
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    app_port = int(os.getenv('APP_PORT', 9000))
+    uvicorn.run(app, host="0.0.0.0", port=app_port)
